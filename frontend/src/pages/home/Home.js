cb460304cd7a4e52320e5925e266c81ee040654f
Yixuan Li <lxd520anna@gmail.com>
2022-01-15 20:23:31 -0500

    add rough typing animation

---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Home.css";
+
+export const Home = () => {
+    return (
+        <div className="title-container">
+            <div className="typed-out">Learn to Code</div>
+        </div>
+    );
+};
+
+export default Home;
