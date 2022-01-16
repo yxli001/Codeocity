3cb4a4361d6039639a769b4986f0b5648e3049ae
Yixuan Li <lxd520ANNA@gmail.com>
2022-01-15 20:42:12 -0500

    Revert "add rough typing animation"
    This reverts commit cb460304cd7a4e52320e5925e266c81ee040654f.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
+import Home from "./pages/home-page/Home";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
-    return (
-        <Router>
-            <div className="navbar">
-                <Navbar />
-            </div>
+  return (
+    <Router>
+      <div className="navbar">
+        <Navbar />
+      </div>
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-    );
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
