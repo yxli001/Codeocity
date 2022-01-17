70b04640a23262c5774dc26e393480616a6cc62f
Yixuan Li <lxd520ANNA@gmail.com>
2022-01-17 12:11:39 -0500

    add mission page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home-page/Home";
 import Navbar from "./components/navbar/Navbar";
 import Courses from "./pages/courses/course-wrapper/Courses";
+import Mission from "./pages/mission/Mission";
 
 // Yixuan: forms, second page, home page
 // Brian: navbar, courses, and meet the team
 
 function App() {
-  return (
-    <Router>
-      <div className="navbar">
-        <Navbar />
-      </div>
+    return (
+        <Router>
+            <div className="navbar">
+                <Navbar />
+            </div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-      </Routes>
-    </Router>
-  );
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/mission" element={<Mission />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/mission/Mission.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import "./Mission.css";
+import personCoding from "../../images/person-coding.jpg";
+import dreamBig from "../../images/dream-big.jpg";
+
+const Mission = () => {
+    return (
+        <div>
+            <div className="mission-container">
+                <div className="top">
+                    <div className="mission-text-container">
+                        <h2 className="mission-text-title">Learn to code</h2>
+                        <p className="mission-text-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vestibulum ornare blandit dictum. Pellentesque
+                            eu nisl elit. Integer vitae sapien id augue congue
+                            commodo. Integer ut tempor ex, at convallis lectus.
+                            Nullam diam est, pretium in fringilla porta, cursus
+                            ac massa. Mauris sit amet fermentum augue, id tempus
+                            mauris. Quisque eget auctor purus. Curabitur a
+                            ultrices justo. Quisque tincidunt ultricies
+                            porttitor. Maecenas ultricies lorem in justo
+                            egestas, porttitor fermentum tellus pulvinar.
+                        </p>
+                    </div>
+                    <img className="mission-image" src={personCoding} alt="" />
+                </div>
+                <div className="bottom">
+                    <div className="mission-text-container">
+                        <h2 className="mission-text-title">
+                            Turn dreams into reality
+                        </h2>
+                        <p className="mission-text-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vestibulum ornare blandit dictum. Pellentesque
+                            eu nisl elit. Integer vitae sapien id augue congue
+                            commodo. Integer ut tempor ex, at convallis lectus.
+                            Nullam diam est, pretium in fringilla porta, cursus
+                            ac massa. Mauris sit amet fermentum augue, id tempus
+                            mauris. Quisque eget auctor purus. Curabitur a
+                            ultrices justo. Quisque tincidunt ultricies
+                            porttitor. Maecenas ultricies lorem in justo
+                            egestas, porttitor fermentum tellus pulvinar.
+                        </p>
+                    </div>
+                    <img className="mission-image" src={dreamBig} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Mission;
