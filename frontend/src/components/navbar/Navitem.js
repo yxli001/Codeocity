325f84b9cee0c8e800061cb428802861f86e8cc6
Yixuan Li <lxd520ANNA@gmail.com>
2022-01-17 09:59:56 -0500

    responsive home page

--- frontend/src/components/navbar/Navitem.js
+++ frontend/src/components/navbar/Navitem.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 export const Navitem = (props) => {
-  const { url, name } = props;
+    const { url, name } = props;
 
-  return (
-    <div className="nav-item-container">
-      <Link to={url} className="nav-item">
-        <h4 class="link">{name}</h4>
-      </Link>
-    </div>
-  );
+    return (
+        <div className="nav-item-container">
+            <Link to={url} className="nav-item">
+                <h4 className="link">{name}</h4>
+            </Link>
+        </div>
+    );
 };
 
 export default Navitem;
