765d0845f7345935d673e0573424dab5b2a650d1
Brian Mason <brian.kw.mason@gmail.com>
2022-01-17 09:55:27 -0500

    Fixed navbar

--- frontend/src/components/navbar/NavAnimationHandler.js
+++ frontend/src/components/navbar/NavAnimationHandler.js
@@ -3,7 +3,6 @@ const navAnimationHandler = () => {
   const navLinks = document.querySelectorAll(".nav-links li");
   const burger = document.querySelector(".burger");
   nav.classList.toggle("nav-active");
-
   navLinks.forEach((link, index) => {
     if (link.style.animation) {
       // link.style.animation = `navLinkDisappear 0.4s ease backwards ${
