658ef7d461117ea122914d712b412278b05570a5
Brian Mason <brian.kw.mason@gmail.com>
2022-02-22 21:11:33 -0500

    Added more to bio

--- frontend/src/data/Employees.js
+++ frontend/src/data/Employees.js
@@ -5,38 +5,38 @@ import colin from "../images/colin_frankel.png";
 import elizabeth from "../images/elizabeth_yamamoto.jpeg";
 
 const employees = [
-    {
-        id: "1",
-        name: "Brian Mason",
-        roles: ["Co-founder", "Web Dev 2"],
-        description:
-            "I am a sophomore at Walt Whitman Highschool and have been coding for about five years now. I began with website development and then diversified into other languages like Python, Java, and Kotlin! ",
-        image: brian,
-    },
-    {
-        id: "2",
-        name: "Yixuan Li",
-        roles: ["Co-founder", "Web Dev 1", "Java"],
-        description:
-            "I am a sophomore currently attending Montgomery Virtual Academy. I've been coding for about 4 years and have taught myself programming languages such as Java, Python, JavaScript, and Swift.",
-        image: yixuan,
-    },
-    {
-        id: "3",
-        name: "Colin Frankel",
-        roles: ["Web Dev 1"],
-        description:
-            "I am a Sophomore at Walt Whitman Highschool. I have been coding for about four years. I started learning HTML and CSS and then wanted more flexibility in web development and wanted to branch out and learned JavaScript.",
-        image: colin,
-    },
-    {
-        id: "4",
-        name: "Elizabeth Yamamoto",
-        roles: ["Java"],
-        description:
-            "Elizabeth is a Junior at Walt Whitman who started coding as a freshman. She has almost three years of experience with Java and taught it in the past through another student led organization. Last year, she scored a 5 on the AP Computer Science Principles exam, and she is currently completing Computer Programming 3: Advanced Topics, which emphasizes manipulating data structures and ADTs. She was accepted into the University of Maryland's Bits & Bytes program, which focuses on STEM outreach for women in high school. Outside of school, she enjoys reading philosophy, playing the violin, and watching Snowpiercer and The Expanse with her family.",
-        image: elizabeth,
-    },
+  {
+    id: "1",
+    name: "Brian Mason",
+    roles: ["Co-founder", "Web Dev 2"],
+    description:
+      "I am a sophomore at Walt Whitman High school and have been coding for about five years. I started my journey with Website Development and then moved on to Python after about a year. I am proficient in HTML, CSS, JavaScript, NodeJS, Python, Java, and Kotlin. I am a member of the varsity tennis team and enjoy running in my free time!",
+    image: brian,
+  },
+  {
+    id: "2",
+    name: "Yixuan Li",
+    roles: ["Co-founder", "Web Dev 1", "Java"],
+    description:
+      "I am a sophomore currently attending Montgomery Virtual Academy. I've been coding for about 4 years and have taught myself programming languages such as Java, Python, JavaScript, and Swift.",
+    image: yixuan,
+  },
+  {
+    id: "3",
+    name: "Colin Frankel",
+    roles: ["Web Dev 1"],
+    description:
+      "I am a Sophomore at Walt Whitman Highschool. I have been coding for about four years. I started learning HTML and CSS and then wanted more flexibility in web development and wanted to branch out and learned JavaScript.",
+    image: colin,
+  },
+  {
+    id: "4",
+    name: "Elizabeth Yamamoto",
+    roles: ["Java"],
+    description:
+      "Elizabeth is a Junior at Walt Whitman who started coding as a freshman. She has almost three years of experience with Java and taught it in the past through another student led organization. Last year, she scored a 5 on the AP Computer Science Principles exam, and she is currently completing Computer Programming 3: Advanced Topics, which emphasizes manipulating data structures and ADTs. She was accepted into the University of Maryland's Bits & Bytes program, which focuses on STEM outreach for women in high school. Outside of school, she enjoys reading philosophy, playing the violin, and watching Snowpiercer and The Expanse with her family.",
+    image: elizabeth,
+  },
 ];
 
 export default employees;
