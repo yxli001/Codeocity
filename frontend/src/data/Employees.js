311060bb7a61b523d41c825fde771512d8d7a4ed
Yixuan Li <lxd520ANNA@gmail.com>
2023-01-05 16:13:37 -0500

    employee update

--- frontend/src/data/Employees.js
+++ frontend/src/data/Employees.js
@@ -2,34 +2,41 @@
 import brian from "../images/brian_mason.jpg";
 import yixuan from "../images/yixuan_li_2.JPG";
 import colin from "../images/colin_frankel.png";
-import scollier from "../images/scollier.jpg";
-import sophie from "../images/sophie.jpg";
+import dante from "../images/dante.jpg";
 
 const employees = [
-  {
-    id: 1,
-    name: "Brian Mason",
-    roles: ["Founder", "Web Dev 1", "Intro to JavaScript", "Advanced CSS"],
-    description:
-      "I am a junior at Walt Whitman Highschool and have been coding for about five years now. I began with website development and then diversified into other languages like Python, Java, and Kotlin! Outside of school, I enjoy playing tennis and am on the Whitman Varsity Tennis Team.",
-    image: brian,
-  },
-  {
-    id: 2,
-    name: "Yixuan Li",
-    roles: ["Founder", "Web Dev 1", "Intro to JavaScript", "Advanced CSS"],
-    description:
-      "I am a junior at Walt Whitman High School. I've been coding for about 4 years and have taught myself programming languages such as Java, Python, JavaScript, and Swift. ",
-    image: yixuan,
-  },
-  {
-    id: 3,
-    name: "Colin Frankel",
-    roles: ["Web Dev 1", "Intro to JavaScript"],
-    description:
-      "I am a junior at Walt Whitman Highschool. I have been coding for about four years. I started with learning HTML and CSS and then later learned JavaScript to make content dynamic on my websites!",
-    image: colin,
-  },
+    {
+        id: 1,
+        name: "Brian Mason",
+        roles: ["Founder", "Web Dev 1", "Intro to JavaScript", "Advanced CSS"],
+        description:
+            "I am a junior at Walt Whitman Highschool and have been coding for about five years now. I began with website development and then diversified into other languages like Python, Java, and Kotlin! Outside of school, I enjoy playing tennis and am on the Whitman Varsity Tennis Team.",
+        image: brian,
+    },
+    {
+        id: 2,
+        name: "Yixuan Li",
+        roles: ["Founder", "Web Dev 1", "Intro to JavaScript", "Advanced CSS"],
+        description:
+            "I am a junior at Walt Whitman High School. I've been coding for about 4 years and have taught myself programming languages such as Java, Python, JavaScript, and Swift. ",
+        image: yixuan,
+    },
+    {
+        id: 3,
+        name: "Colin Frankel",
+        roles: ["Web Dev 1", "Intro to JavaScript"],
+        description:
+            "I am a junior at Walt Whitman Highschool. I have been coding for about four years. I started with learning HTML and CSS and then later learned JavaScript to make content dynamic on my websites!",
+        image: colin,
+    },
+    {
+        id: 4,
+        name: "Dante Suzuki",
+        roles: ["Scratch"],
+        description:
+            "I'm a junior at Walt Whitman High School and have been in computer science for about six years. I have been involved in Java, C++, C#, HTML, Python, and Scratch.",
+        image: dante,
+    },
 ];
 
 export default employees;
